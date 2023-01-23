fb580c9e40c22552cc50171242d9d7a7f6dd5301
Rafael Santos <rafaelsaannttos@gmail.com>
2023-01-23 04:41:43 -0300

    done project - routes ans links

--- project-angular-teste/src/app/nova-transferencia/nova-transferencia.component.ts
+++ project-angular-teste/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -2,6 +2,7 @@ import { TransferenciaService } from './../services/transferencia.service';
 import { Transferencia } from './../models/transferencia.model';
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-nova-transferencia",
@@ -16,7 +17,7 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
-  constructor(private service: TransferenciaService) { }
+  constructor(private service: TransferenciaService, private router: Router) { }
 
   transferir() {
     const valorEmitir = { valor: this.valor, destino: this.destino }
@@ -24,6 +25,7 @@ export class NovaTransferenciaComponent {
     this.service.adicionar(valorEmitir).subscribe(resultado => {
       console.log(resultado)
       this.limpaCampos()
+      this.router.navigateByUrl('extrato')
     }, error => {
       console.error(error)
     })
